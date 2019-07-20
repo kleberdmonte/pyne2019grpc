917637ccbb8b32b9879372d2f4969ed65cd70a80
KleberMonte <kleberdarlanmonte@gmail.com>
2019-07-20 11:55:50 -0300

    Alteracoes feitas no Workshop PyNE 2019

--- client/client.py
+++ client/client.py
@@ -3,12 +3,32 @@ import grpc
 import pyne_pb2
 import pyne_pb2_grpc
 
-def SayHello(text):
+def SayHello(**kwargs):
     with grpc.insecure_channel('pyne:3000') as channel:
         stub = pyne_pb2_grpc.GreeterStub(channel)
-        req = pyne_pb2.HelloRequest(name=text)
+        req = pyne_pb2.HelloRequest(**kwargs)
         response = stub.SayHello(req)
         print(response.greetings)
 
+
+def CalcSum(**kwargs):
+    with grpc.insecure_channel('pyne:3000') as channel:
+        stub = pyne_pb2_grpc.CalculatorStub(channel)
+        req = pyne_pb2.SumRequest(**kwargs)
+        response = stub.Sum(req)
+        print(f'Result = {response.result}')
+
+
+def UtcNow(**kwargs):
+    with grpc.insecure_channel('pyne:3000') as channel:
+        stub = pyne_pb2_grpc.NtpStub(channel)
+        req = pyne_pb2.UtcNowRequest(**kwargs)
+        
+        for response in stub.UtcNow(req):
+            print(f'Utc Now = {response.yyMMddHHmmss}')
+
 if __name__ == '__main__':
-    SayHello('dev')
+    # CalcSum(value=(10, -5, 5, 10))
+    # SayHello(a='teste', b=25)
+    UtcNow(interval_in_secs=1, max_amount=5)
+
--- server/service.py
+++ server/service.py
@@ -1,4 +1,7 @@
 from concurrent import futures
+from functools import reduce
+from datetime import datetime
+import time
 
 import time
 
@@ -10,11 +13,45 @@ import pyne_pb2_grpc
 class Greeter(pyne_pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
         print(f'{request.name} said Hello.')
-        return pyne_pb2.HelloResponse(greetings=f'Hello, {request.name}.')
+        print(f'{request.age} years old.')
+        return pyne_pb2.HelloResponse(greetings=f'Hello, {request.name}. I heard you are {request.age}.')
+
+
+class Calculator(pyne_pb2_grpc.CalculatorServicer):
+    def Sum(self, request, context):
+        # print(f'a = {request.a}.')
+        print(f'value = {request.value}.')
+
+        if len(request.value) > 1:
+            result = reduce(lambda x, y: x + y, request.value)
+        else:
+            result = 0
+
+        return pyne_pb2.SumResponse(result=result)
+
+
+class Ntp(pyne_pb2_grpc.NtpServicer):
+    def UtcNow(self, request, context):
+        print(f'Interval = {request.interval_in_secs}.')
+        print(f'Max Amount = {request.max_amount}.')
+
+        for _ in range(request.max_amount):
+            time.sleep(request.interval_in_secs)
+
+            now = datetime.utcnow()
+            format_now = now.strftime('%Y%-M-%d %H:%m:%s')
+            print(format_now)
+
+            yield pyne_pb2.UtcNowResponse(yyMMddHHmmss=format_now)
+            # yield format_now
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     pyne_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    pyne_pb2_grpc.add_CalculatorServicer_to_server(Calculator(), server)
+    pyne_pb2_grpc.add_NtpServicer_to_server(Ntp(), server)
+
     server.add_insecure_port('0.0.0.0:3000')
     server.start()
     print('Waiting for requests...')
